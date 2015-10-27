5b0fab3c55acb0294d94becc1f9181576a5241cf
Charlie Phillips <charliebphillips@gmail.com>
2015-10-27 02:01:21 -0400

    finished problem 2 in perl

--- problem2.pl
+++ problem2.pl
@@ -1,19 +1,20 @@
 #!/usr/bin/perl
 
 use LWP::Simple;
+use JSON qw( decode_json );
 
 open(FILE, $ARGV[0]) or die "Couldn't open file $ARGV[0], $!";
 
 while(<FILE>){
-#   print "$_";
    my $symbol = $_;
    chomp $symbol;
    my $url = "http://finance.yahoo.com/webservice/v1/symbols/$symbol/quote?format=json&view=detail";
-   my $response = get $url;
+   my $response = get( $url );
    die 'Error getting $url' unless defined $response;
-#   print "$response";
-   my $price = 1;
-   my $change = 2;
+
+   my $decoded_json = decode_json( $response );
+   my $price = $decoded_json->{'list'}{'resources'}[0]{'resource'}{'fields'}{'price'};
+   my $change = $decoded_json->{'list'}{'resources'}[0]{'resource'}{'fields'}{'change'};
    print "$symbol $price $change\n";   
 }
 
