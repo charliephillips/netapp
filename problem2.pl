abb6cebad49ddf0184811e5835214a0416ed7fad
Charlie Phillips <charliebphillips@gmail.com>
2015-10-27 10:51:00 -0400

    Fixing formatting

--- problem2.pl
+++ problem2.pl
@@ -14,8 +14,10 @@ while(<FILE>){
 
    my $decoded_json = decode_json( $response );
    my $price = $decoded_json->{'list'}{'resources'}[0]{'resource'}{'fields'}{'price'};
+   $price = sprintf "%.2f", $price;
    my $change = $decoded_json->{'list'}{'resources'}[0]{'resource'}{'fields'}{'change'};
-   print "$symbol $price $change\n";   
+   $change = sprintf "%.2f", $change;
+   printf("%-10s %-10s %-10s\n", $symbol, $price, $change);
 }
 
 
